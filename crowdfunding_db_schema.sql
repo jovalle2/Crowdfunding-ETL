f97a189b273018ab15bad17e2bf995819ce34096
jonathan-ovalle <revenant-umbra@users.noreply.github.com>
2023-04-16 01:00:38 -0600

    Uploading all files

---
+++ crowdfunding_db_schema.sql
@@ -0,0 +1,15 @@
+CREATE TABLE "backers" (
+    "backer_id"  varchar(10)  NOT NULL,
+	"cf_id" int   NOT NULL,
+    "first_name" varchar(50)   NOT NULL,
+    "last_name" varchar(50)   NOT NULL,
+    "email" varchar(50)   NOT NULL
+  
+);
+
+ALTER TABLE "backers" ADD CONSTRAINT "fk_backers_cf_id" FOREIGN KEY("cf_id")
+REFERENCES "campaign" ("cf_id");
+
+SELECT *
+FROM backers
+FETCH NEXT 10 ROWS ONLY
